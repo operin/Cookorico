9b5670da1bc91894764ec6c258615d5d5c35f88d
operin <operin69@gmail.com>
2015-10-12 22:51:28 +0000

    Merge branch 'master' of https://github.com/Cookorico/Cookorico

--- src/main/java/fil/iagl/cookorico/controller/HomeController.java
+++ src/main/java/fil/iagl/cookorico/controller/HomeController.java
@@ -1,9 +1,28 @@
 package fil.iagl.cookorico.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import fil.iagl.cookorico.entity.Member;
+import fil.iagl.cookorico.service.MemberService;
+import fil.iagl.cookorico.wrapper.LoginWrapper;
 
 @RestController
 public class HomeController {
 
-
+	@Autowired
+	MemberService memberService;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public Member login(@RequestBody LoginWrapper wrapper){
+		System.out.println(wrapper.getUsername());
+		System.out.println(wrapper.getPassword());
+		Member member = memberService.getMember(wrapper.getUsername(), wrapper.getPassword());
+		System.out.println(member);
+		return member;
+	}
+	
 }
